b67324610b15e198942ca51b0cf8021eedfa8fa8
Eric Liu <ericyl.us@gmail.com>
2021-10-31 11:23:33 -0700

    fix(types): remove update from focusKey action

--- types/focus-key.d.ts
+++ types/focus-key.d.ts
@@ -1,11 +1,8 @@
 import { FocusKeyProps } from "./FocusKey.svelte";
 
-type FocusKeyOptions = Pick<FocusKeyProps, "key">;
-
 export function focusKey(
   element: HTMLElement,
-  options?: FocusKeyOptions
+  options?: Pick<FocusKeyProps, "key">
 ): {
-  update: (options?: FocusKeyOptions) => void;
   destroy: () => void;
 };
