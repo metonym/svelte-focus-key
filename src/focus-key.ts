9dc48a1e4cd4affd7e7557596dcf5a180ac887ca
metonym <ericyl.us@gmail.com>
2022-02-07 17:11:57 -0800

    fix(focus-key): action should reflection updated options (#3)

--- src/focus-key.ts
+++ src/focus-key.ts
@@ -7,13 +7,13 @@ export function focusKey(
   element: HTMLInputElement | HTMLTextAreaElement,
   options: FocusKeyOptions = {}
 ) {
-  const key = options.key || "/";
-  const keys = Array.isArray(key) ? key : [key];
-  const selectText = options.selectText === true;
+  let key = options.key || "/";
+  let keys = Array.isArray(key) ? key : [key];
+  let selectText = options.selectText === true;
+
   const keydown = (e: KeyboardEvent) => {
     if (
       keys.some((key) => key === e.key) &&
-      element != null &&
       document.activeElement?.tagName === "BODY" &&
       document.activeElement !== element
     ) {
@@ -26,6 +26,12 @@ export function focusKey(
   document.body.addEventListener("keydown", keydown);
 
   return {
+    update(options: FocusKeyOptions = {}) {
+      key = options.key || "/";
+      keys = Array.isArray(key) ? key : [key];
+      selectText = options.selectText === true;
+      element.selectionStart = element.selectionEnd;
+    },
     destroy() {
       document.body.removeEventListener("keydown", keydown);
     },
