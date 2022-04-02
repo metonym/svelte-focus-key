6ba49294ee72c37d4c7e3367e63cba7c12fb8989
Eric Liu <ericyl.us@gmail.com>
2022-04-02 07:02:07 -0700

    refactor(focus-key): use Action type to type `focusKey`

--- src/focus-key.ts
+++ src/focus-key.ts
@@ -1,12 +1,16 @@
+import type { Action } from "svelte/action";
+
 interface FocusKeyOptions {
   key?: string | string[];
   selectText?: boolean;
 }
 
-export function focusKey(
-  element: HTMLInputElement | HTMLTextAreaElement,
-  options: FocusKeyOptions = {}
-) {
+type FocusKeyAction = Action<
+  HTMLInputElement | HTMLTextAreaElement,
+  FocusKeyOptions
+>;
+
+export const focusKey: FocusKeyAction = (element, options = {}) => {
   let key = options.key || "/";
   let keys = Array.isArray(key) ? key : [key];
   let selectText = options.selectText === true;
@@ -26,7 +30,7 @@ export function focusKey(
   document.body.addEventListener("keydown", keydown);
 
   return {
-    update(options: FocusKeyOptions = {}) {
+    update(options = {}) {
       key = options.key || "/";
       keys = Array.isArray(key) ? key : [key];
       selectText = options.selectText === true;
@@ -36,4 +40,4 @@ export function focusKey(
       document.body.removeEventListener("keydown", keydown);
     },
   };
-}
+};
